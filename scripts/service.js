d662f567d61763d3f2d32222b94f771706eee54d
anylots <842567494@qq.com>
2022-09-03 16:26:55 +0800

    Update service.js

--- scripts/service.js
+++ scripts/service.js
@@ -98,7 +98,7 @@ async function swapExactTokensForTokens() {
 }
 
 async function test() {
-    let privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    let privateKey = "";
     let wallet = new ethers.Wallet(privateKey);
 
     // Connect a wallet to mainnet
